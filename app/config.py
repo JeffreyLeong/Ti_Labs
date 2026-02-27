59d69e6034b1e291fef94f3de8397b42dd5eaf03
dollarortwo <jleong001@gmail.com>
2026-02-27 15:06:09 -0800

    Update categories: replace Food with Groceries, add Eating Out

--- app/config.py
+++ app/config.py
@@ -8,4 +8,4 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = f"postgresql://ti_labs_user:{os.getenv('DB_PASSWORD')}@localhost/ti_labs_db"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://admin:{os.getenv('DB_PASSWORD')}@localhost/ti_labs_new"
